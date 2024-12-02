1358812de6103707821e2c183ea074ffa6ecb48e
ozReilly <710339974@qq.com>
2024-12-02 15:20:24 +0800

    Update test-highload-query-id.js

--- src/test-highload-query-id.js
+++ src/test-highload-query-id.js
@@ -1,40 +1,43 @@
 const TonWeb = require("./index");
 const HighloadQueryId = TonWeb.HighloadWallets.HighloadQueryId;
 
-if (HighloadQueryId.fromSeqno(0n).toSeqno() !== 0n) throw new Error();
+if (HighloadQueryId.fromSeqno(BigInt(0)).toSeqno() !== BigInt(0))
+  throw new Error();
 
-const i = HighloadQueryId.fromSeqno(1022n);
-if (i.toSeqno() !== 1022n) throw new Error();
-const i2 = HighloadQueryId.fromSeqno(1023n);
-if (i2.toSeqno() !== 1023n) throw new Error();
-const i3 = HighloadQueryId.fromSeqno(1024n);
-if (i3.toSeqno() !== 1024n) throw new Error();
-const i4 = HighloadQueryId.fromSeqno(8380415n);
-if (i4.toSeqno() !== 8380415n) throw new Error();
-
+const i = HighloadQueryId.fromSeqno(BigInt(1022));
+if (i.toSeqno() !== BigInt(1022)) throw new Error();
+const i2 = HighloadQueryId.fromSeqno(BigInt(1023));
+if (i2.toSeqno() !== BigInt(1023)) throw new Error();
+const i3 = HighloadQueryId.fromSeqno(BigInt(1024));
+if (i3.toSeqno() !== BigInt(1024)) throw new Error();
+const i4 = HighloadQueryId.fromSeqno(BigInt(8380415));
+if (i4.toSeqno() !== BigInt(8380415)) throw new Error();
 
 let queryId = new HighloadQueryId();
 console.log(queryId.getQueryId(), queryId.hasNext());
 
-const MAX = (2n ** 13n) * 1023n - 2n;
+const MAX = BigInt(2) ** BigInt(13) * BigInt(1023) - BigInt(2);
 for (let i = 0; i < MAX; i++) {
-    queryId = queryId.getNext();
+  queryId = queryId.getNext();
 
-    const q = queryId.getQueryId();
-    const q2 = HighloadQueryId.fromQueryId(q);
+  const q = queryId.getQueryId();
+  const q2 = HighloadQueryId.fromQueryId(q);
 
-    if (queryId.getShift() !== q2.getShift()) throw new Error()
-    if (queryId.getBitNumber() !== q2.getBitNumber()) throw new Error()
-    if (q2.getQueryId() !== q) throw new Error();
+  if (queryId.getShift() !== q2.getShift()) throw new Error();
+  if (queryId.getBitNumber() !== q2.getBitNumber()) throw new Error();
+  if (q2.getQueryId() !== q) throw new Error();
 
-    const q3 = HighloadQueryId.fromShiftAndBitNumber(queryId.getShift(), queryId.getBitNumber());
-    if (queryId.getShift() !== q3.getShift()) throw new Error()
-    if (queryId.getBitNumber() !== q3.getBitNumber()) throw new Error()
-    if (q3.getQueryId() !== q) throw new Error();
+  const q3 = HighloadQueryId.fromShiftAndBitNumber(
+    queryId.getShift(),
+    queryId.getBitNumber()
+  );
+  if (queryId.getShift() !== q3.getShift()) throw new Error();
+  if (queryId.getBitNumber() !== q3.getBitNumber()) throw new Error();
+  if (q3.getQueryId() !== q) throw new Error();
 
-    if (!queryId.hasNext()) {
-        console.log('END')
-    }
+  if (!queryId.hasNext()) {
+    console.log("END");
+  }
 }
 console.log(queryId.shift);
 console.log(queryId.bitnumber);
